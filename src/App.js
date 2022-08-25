d67aee317c5e25df39534aef877bdba63c5816c0
Muskulee <muskulee@gmail.com>
2022-08-25 11:35:22 +0200

    Nav Routes Created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai";
 import { Route, Routes } from "react-router-dom";
+import CreatePost from "./components/CreatePost";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+import UpdatePost from "./components/UpdatePost";
 
 export default function App() {
   const [closedNav, setClosedNav] = useState(false);
@@ -33,6 +37,9 @@ export default function App() {
         <div className="max-w-screen-lg mx-auto">
            <Routes>
               <Route path="/" element={<Home />}/>
+              <Route path="/create-post" element={<CreatePost />}/>
+              <Route path="/update-post" element={<UpdatePost />}/>
+              <Route path="*" element={<NotFound />}/>
            </Routes>
         </div>
       </div>
