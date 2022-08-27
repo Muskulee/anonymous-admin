bd1873fa98ec6af66f960590ab057b5737086234
Muskulee <muskulee@gmail.com>
2022-08-28 00:59:29 +0200

    States and Notifications for create post fixed

--- src/api/post.js
+++ src/api/post.js
@@ -48,3 +48,18 @@ export const searchPost = async (query) => {
     return { error: error.message || error };
   }
 };
+
+export const uploadImage = async (formData) => {
+  try {
+    // const { data } = await client(`/post/posts?pageNo=${pageNo}&limit=${limit}`);
+    const { data } = await client.post(`${baseUrl}/post/upload-image`, formData);
+    //   console.log('client', client)
+    return data;
+  } catch (error) {
+    const { response } = error;
+    if (response?.data) {
+      return response.data;
+    }
+    return { error: error.message || error };
+  }
+};
--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ImSpinner11,
   ImEye,
   ImFilePicture,
   ImFilesEmpty,
+  ImSpinner3,
 } from "react-icons/im";
+import { uploadImage, uploadImmage } from "../api/post";
+import { useNotification } from "../context/NotificationProvider";
 
 const mdRules = [
   {
@@ -17,7 +20,7 @@ const mdRules = [
   },
   {
     title: " Image",
-    rule: "![image alt](http://image_url.com",
+    rule: "![image alt](http://image_url.com)",
   },
   {
     title: " Link",
@@ -25,7 +28,101 @@ const mdRules = [
   },
 ];
 
+const defaultPost = {
+  title: "",
+  thumbnail: "",
+  featured: false,
+  content: "",
+  tags: "",
+  meta: "",
+  likeCount: "",
+  commentCount: "",
+};
+
 export default function CreatePost() {
+  const [postInfo, setPostInfo] = useState({ ...defaultPost });
+  const [selectedThumbnailURL, setSelectedThumbnailURL] = useState("");
+  const [imageURLToCopy, setImageURLToCopy] = useState("");
+  const [imageUploading, setImageUploading] = useState("");
+
+  const { updateNotification } = useNotification();
+
+  const handleChange = ({ target }) => {
+    const { value, name, checked } = target;
+
+    if (name === "thumbnail") {
+      const file = target.files[0];
+      if (!file.type?.includes("image")) {
+        return alert("This is not an image file");
+      }
+
+      setPostInfo({ ...postInfo, thumbnail: value });
+      return setSelectedThumbnailURL(URL.createObjectURL(file));
+    }
+    if (name === "thumbnail") {
+      const file = target.files[0];
+      if (!file.type?.includes("image")) {
+        return alert("This is not an image file");
+      }
+
+      setPostInfo({ ...postInfo, thumbnail: value });
+      return setSelectedThumbnailURL(URL.createObjectURL(file));
+    }
+
+    if (name === "featured") {
+      return setPostInfo({ ...postInfo, [name]: checked });
+    }
+
+    if (name === "tags") {
+      const newTags = tags.split(", ");
+      if (newTags.length > 4) updateNotification("warning", "Only First Four Tags Will Be Used!");
+      // console.log("");
+    }
+    if (name === "meta" && meta.length > 150) {
+      return setPostInfo({ ...postInfo, meta: value.substring(0, 149) });
+    }
+
+    setPostInfo({ ...postInfo, [name]: value });
+  };
+
+  const handleImageUpload = async ({ target }) => {
+    const file = target.files[0];
+
+    if (imageUploading) return;
+
+    if (!file.type?.includes("image")) {
+      return updateNotification("error", "This is not an image!");
+    }
+
+    setImageUploading(true);
+    const formData = new FormData();
+    formData.append("image", file);
+
+    const { error, image } = await uploadImage(formData);
+    setImageUploading(false);
+
+    if (error) return console.log(error);
+
+    setImageURLToCopy(image);
+  };
+
+  const handleOnCopy = () => {
+    const textToCopy = `![Add image description](${imageURLToCopy})`;
+    navigator.clipboard.writeText(textToCopy);
+  };
+
+  const {
+    title,
+    author,
+    createdAt,
+    content,
+    featured,
+    tags,
+    meta,
+    likeCount,
+    postCount,
+  } = postInfo;
+
   return (
     <form className="p-2 flex">
       <div className="w-9/12 h-screen space-y-3 flex flex-col">
@@ -36,11 +133,17 @@ export default function CreatePost() {
           </h1>
 
           <div className="flex items-center space-x-5">
-            <button className="flex items-center space-x-2 px-3 ring-1 ring-teal-500 rounded h-10 text-teal-500 hover:text-white hover:bg-teal-500 transition">
+            <button
+              type="button"
+              className="flex items-center space-x-2 px-3 ring-1 ring-teal-500 rounded h-10 text-teal-500 hover:text-white hover:bg-teal-500 transition"
+            >
               <ImSpinner11 />
               <span> Reset</span>
             </button>
-            <button className="flex items-center space-x-2 px-3 ring-1 ring-teal-500 rounded h-10 text-teal-500 hover:text-white hover:bg-teal-500 transition">
+            <button
+              type="button"
+              className="flex items-center space-x-2 px-3 ring-1 ring-teal-500 rounded h-10 text-teal-500 hover:text-white hover:bg-teal-500 transition"
+            >
               <ImEye />
               <span> View</span>
             </button>
@@ -50,14 +153,22 @@ export default function CreatePost() {
           </div>
         </div>
         {/* Featured CheckBox  */}
-        <div>
-          <input id="featured" type="checkbox" hidden />
+        <div className="flex">
+          <input
+            name="featured"
+            onChange={handleChange}
+            id="featured"
+            type="checkbox"
+            hidden
+          />
           <label
-            className="flex items-center space-x-2 text-gray-700 cursor-pointer group"
+            className=" select-none flex items-center space-x-2 text-gray-700 cursor-pointer group"
             htmlFor="featured"
           >
             <div className="w-4 h-4 rounded-full border-2 border-gray-700 flex items-center justify-center group-hover:border-teal-500">
-              <div className="w-2 h-2 rounded-full bg-gray-700  group-hover:bg-teal-500" />
+              {featured && (
+                <div className="w-2 h-2 rounded-full bg-gray-700  group-hover:bg-teal-500" />
+              )}
             </div>
             <span className="group-hover:text-teal-500">Featured</span>
           </label>
@@ -65,48 +176,66 @@ export default function CreatePost() {
 
         {/* Title Input */}
 
-        <input
+        {/* <input
+          value={title}
+          name="title"
+          onChange={handleChange}
           type="text"
-          className="text-xl outline-none focus:ring-1 ring-teal-500 rounded p-2 w-full"
+          className="text-xl outline-none focus:ring-1
+           ring-teal-500 rounded p-2 w-full"
           placeholder="Post Title"
-        />
+        /> */}
 
         {/* Image Input */}
 
         <div className="flex space-x-2">
           <div>
-            <input id="image-input" type="file" hidden />
+            <input
+              onChange={handleImageUpload}
+              id="image-input"
+              type="file"
+              hidden
+            />
             <label
               htmlFor="image-input"
               className="flex items-center space-x-2 px-3 ring-1
-       ring-gray-500 rounded h-10 text-gray-500
-        hover:text-white hover:bg-gray-500 
-        transition cursor-pointer"
+                  ring-gray-500 rounded h-10 text-gray-500
+                    hover:text-white hover:bg-gray-500 
+                    transition cursor-pointer"
             >
-              <span> Place Image</span>
-              <ImFilePicture />
+              <span>Place Image</span>
+              {!imageUploading ? (
+                <ImFilePicture />
+              ) : (
+                <ImSpinner3 className="animate-spin" />
+              )}
             </label>
           </div>
 
-          <div
-            className="flex-1 flex bg-gray-400
+          {imageURLToCopy && (
+            <div
+              className="flex-1 flex bg-gray-400
         justify-between rounded overflow-hidden"
-          >
-            <input
-              type="text"
-              value="We buildingbbxhdxb nnk jcjhbd bs db"
-              className="bg-transparent px-2 text-white w-full"
-              disabled
-            />
-            <button
-              className="text-xs flex
-          flex-col items-center justify-center p-1
-          self-stretch bg-gray-700 text-white"
             >
-              <ImFilesEmpty />
-              <span>copy</span>
-            </button>
-          </div>
+              <input
+                type="text"
+                onChange={handleChange}
+                value={imageURLToCopy}
+                className="bg-transparent px-2 text-white w-full"
+                disabled
+              />
+              <button
+                onClick={handleOnCopy}
+                type="button"
+                className="text-xs flex
+                flex-col items-center justify-center p-1
+                self-stretch bg-gray-700 text-white"
+              >
+                <ImFilesEmpty />
+                <span>copy</span>
+              </button>
+            </div>
+          )}
         </div>
 
         <input
@@ -116,6 +245,9 @@ export default function CreatePost() {
         />
 
         <textarea
+          value={content}
+          onChange={handleChange}
+          name="content"
           placeholder="## Mark Down"
           className="resize-none outline-none focus:ring-1 ring-teal-500 
         rounded p-2 w-full flex-1
@@ -124,18 +256,28 @@ export default function CreatePost() {
         ></textarea>
         {/* tags input */}
         <div>
-          <label htmlFor="tags">Tags</label>
+          <label className="text-gray-500" htmlFor="tags">
+            Tags
+          </label>
           <input
+            value={tags}
+            onChange={handleChange}
+            name="tags"
             type="text"
             id="tags"
             className=" outline-none focus:ring-1 ring-teal-500 rounded p-2 w-full"
-            placeholder="Post Title"
+            placeholder="Tag One, Tag Two"
           />
         </div>
         {/* Text area */}
         <div>
-          <label htmlFor="meta">Meta Description</label>
+          <label className="text-gray-500" htmlFor="meta">
+            Meta Description {meta.length} / 150
+          </label>
           <textarea
+            onChange={handleChange}
+            value={meta}
+            name="meta"
             id="meta"
             placeholder="Meta Description"
             className="resize-none outline-none focus:ring-1 ring-teal-500 rounded p-2 w-full font-semibold h-28"
@@ -151,33 +293,57 @@ export default function CreatePost() {
           Thumbnail
         </h1>
         <div>
-          <input type="file" hidden id="thumbnail" />
+          <input
+            name="thumbnail"
+            type="file"
+            onChange={handleChange}
+            hidden
+            id="thumbnail"
+          />
           <label className="cursor-pointer" htmlFor="thumbnail">
-            <div
-              className="border border-dashed
-           border-gray-500 aspect-video text-gray-500
-           flex flex-col justify-center items-center"
-            >
-              <span>Select Thumbnail </span>
-              <span className="text-xs">Recommended Size </span>
-              <span className="text-xs">1280 * 720 </span>
-            </div>
+            {selectedThumbnailURL ? (
+              <img
+                src={selectedThumbnailURL}
+                className="aspect-video shadow-sm rounded"
+                alt=""
+              />
+            ) : (
+              <div
+                className="border border-dashed
+             border-gray-500 aspect-video text-gray-500
+             flex flex-col justify-center items-center"
+              >
+                <span>Select Thumbnail </span>
+                <span className="text-xs">Recommended Size </span>
+                <span className="text-xs">1280 * 720 </span>
+              </div>
+            )}
           </label>
         </div>
+
+        {/* Markdown Rules */}
 
         <div className="bg-white absolute top-1/2 -translate-y-1/2 px-2 py-4 rounded">
           <h1 className="font-semibold text-center">General Markdown Rules</h1>
 
           <ul className="space-y-2">
-            {mdRules.map(({title, rule })=> {
-              return <li key={title}> 
-                <p className="font-semibold  text-gray-500"> {title}</p>
-                <p className="font-semibold  text-gray-700 pl-2 font-mono">{rule}</p>
-              </li>
+            {mdRules.map(({ title, rule }) => {
+              return (
+                <li key={title}>
+                  <p className="font-semibold  text-gray-500"> {title}</p>
+                  <p className="font-semibold  text-gray-700 pl-2 font-mono">
+                    {rule}
+                  </p>
+                </li>
+              );
             })}
-
             <li className="text-center text-teal-500">
-              <a href="https://www.markdownguide.org/basic-syntax/" taget="_blank">Find Out More</a>
+              <a
+                href="https://www.markdownguide.org/basic-syntax/"
+                taget="_blank"
+              >
+                Find Out More
+              </a>
             </li>
           </ul>
         </div>
