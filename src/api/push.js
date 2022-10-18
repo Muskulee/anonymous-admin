36318470517d3ac99461f893c23ce31acaf8f903
Muskulee <muskulee@gmail.com>
2022-10-18 21:50:26 +0200

    Finally

--- src/api/push.js
+++ src/api/push.js
@@ -7,7 +7,7 @@ export const sendPushMessage = async (formData) => {
   try {
     // const { data } = await client(`/post/posts?pageNo=${pageNo}&limit=${limit}`);
     const { data } = await client.post(`${baseUrl}/push/sendpush`, formData);
-    console.log('client', data)
+    // console.log('client', data)
     return data;
   } catch (error) {
     const { response } = error;
